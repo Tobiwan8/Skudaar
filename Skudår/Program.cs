c239cc08573e19c5f7c31353824aa83b4028dea9
Unicow8 <tobiaslarsen93@gmail.com>
2022-04-21 14:49:57 +0200

    Færdigt program til ansøgning

---
+++ Skudår/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skudår
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indtast årstal: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            int leapyear = Leap(year);
+
+            if (year <= 1000)
+                Console.WriteLine("-1");
+            else if (leapyear == 1)
+                Console.WriteLine("leapyear, " + leapyear);
+            else
+                Console.WriteLine("not a leapyear, " + leapyear);
+
+
+            Console.ReadKey();
+        }
+
+        static int Leap(int num)
+        {
+            int result = num % 4;
+
+            if (result == 0)
+                return 1;
+            else
+                return -1;
+        }
+    }
+}
